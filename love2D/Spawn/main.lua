e6f59cc70c24b5f64ce44722f99f877ae842044d
Shams Parvez Arka <parvez6826@gmail.com>
2023-03-24 16:05:02 +0600

    windfield support added

--- love2D/Spawn/main.lua
+++ love2D/Spawn/main.lua
@@ -1,4 +1,5 @@
 require "Constants"
+local wf = require"windfield"
 
 function love.load()
 	love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
@@ -6,20 +7,26 @@ function love.load()
 		resizable = false,
 		vsync = true
 	})
-end
 
-vector_x = {}
-vector_y = {}
+	world = wf.newWorld(0, 0, true)
+	world:setGravity(0, 512)
+	world:setQueryDebugDrawing(true)
+
+	ground = world:newRectangleCollider(0, WINDOW_HEIGHT - 30, WINDOW_WIDTH, 30)
+	ground:setType("static")
+
+	colliders = {}
+end
 
 function love.update(dt)
 	if (love.mouse.isDown(1)) then
-		table.insert(vector_x, love.mouse.getX())
-		table.insert(vector_y, love.mouse.getY())
+		local collider = world:newRectangleCollider(love.mouse.getX() - 30 / 2, love.mouse.getY() - 30 / 2, 30, 30)
+		collider:setType("dynamic")
+		table.insert(colliders, collider)
 	end
+	world:update(dt)
 end
 
 function love.draw()
-	for i = 1, #vector_x do
-		love.graphics.circle("fill", vector_x[i], vector_y[i], RADIUS)
-	end
+	world:draw()
 end
